8ad76f169cc20ce956e4daf67b1e02abfea2139c
Andreas Albert <albert@physik.rwth-aachen.de>
2017-07-14 18:12:51 +0200

    Improve fitting.

--- fit_efficiency.py
+++ fit_efficiency.py
@@ -8,7 +8,7 @@ from ROOT import RooFit, RooRealVar, RooGaussian, RooDataSet, RooArgList, RooTre
 from cms_plot import create_tdr_style,add_text
 import sys
 
-MZ = 91
+MZ = 90
 create_tdr_style(True)
 
 
@@ -24,11 +24,27 @@ def do_single_fit(h2d, run, sigpdf=None, bkgpdf=None):
 
 
    ### Signal PDF
+   delta = max(2,h.GetRMS())
    if(not sigpdf):
-      delta = max(2,h.GetRMS())
       mean =  RooRealVar("mean","Peak Mass",MZ,MZ-5*delta,MZ+5*delta,"GeV")
       sigma = RooRealVar("sigma","Peak Width",1,0.1,50,"GeV")
       sig = RooBreitWigner("sig","sig",mass,mean,sigma);
+   elif(sigpdf=="bwcrystal"):
+      # Signal
+      mean =  RooRealVar("mean","Peak Mass",MZ,MZ-3*delta,MZ+3*delta,"GeV")
+      sigma = RooRealVar("sigma","Peak Width",1,0.001,10,"GeV")
+      bw = RooBreitWigner("bw","bw",mass,mean,sigma);
+   
+      # Resolution
+      resolution_mean =  RooRealVar("resolution_mean","resolution_mean",0,"GeV")
+      resolution_width = RooRealVar("resolution_width","resolution_width",1,0.0001,10,"GeV")
+      resolution_a = RooRealVar("a","a",1,0,4)
+      resolution_n = RooRealVar("n","n",2,0,200)
+
+      resolution = RooCBShape("CBShape", "Cystal Ball Function", mass, resolution_mean, resolution_width, resolution_a, resolution_n);
+
+      # Convolution
+      sig = RooFFTConvPdf("sig","sig",mass,bw,resolution)
 
    ### Background PDF
    y1 = h.GetBinContent(1)
@@ -38,9 +54,10 @@ def do_single_fit(h2d, run, sigpdf=None, bkgpdf=None):
 
 
    if(not bkgpdf):
-      alpha= RooRealVar("alpha","alpha",sy,-1e3,1e3) ;
-      beta= RooRealVar("beta","beta",dy / dx,-100./dx,100/dx) ;
-      #~ beta= RooRealVar("beta","beta",0) ;
+      #~ alpha= RooRealVar("alpha","alpha",sy,-1e3,1e3) ;
+      alpha= RooRealVar("alpha","alpha",0) ;
+      #~ beta = RooRealVar("beta","beta",dy / dx,-100./dx,100/dx) ;
+      beta= RooRealVar("beta","beta",0) ;
       bkg = RooPolynomial("bkg","Background",mass,RooArgList(alpha,beta),0) ;
    elif(bkgpdf.lower()=="cms"):
       alpha= RooRealVar("alpha","alpha",sy,0,100) ;
@@ -58,8 +75,8 @@ def do_single_fit(h2d, run, sigpdf=None, bkgpdf=None):
 #~
    dh = RooDataHist("dh","dh",RooArgList(mass),h,1) ;
    #~ fit_result = model.fitTo(dh,RooFit.Save(1))
-   #~ model.fitTo(dh,RooFit.SumW2Error(r.kTRUE))
-   model.chi2FitTo(dh,RooLinkedList())
+   model.fitTo(dh,RooFit.Save(1))
+   #~ model.chi2FitTo(dh,RooLinkedList())
 
    histos = []
    histos.append(h)
@@ -68,15 +85,34 @@ def do_single_fit(h2d, run, sigpdf=None, bkgpdf=None):
    if(not os.path.exists(outdir)):
       os.makedirs(outdir)
 
-   frame = mass.frame() ;
+   frame = mass.frame(RooFit.Title(h2d.GetTitle().replace("h_ee_mass_",""))) ;
    dh.plotOn(frame)
    frame.Draw()
-   c1.SaveAs('{OUT}/run{RUN}_{TITLE}_nofit.pdf'.format(OUT=outdir,RUN=run,TITLE=h2d.GetTitle()))
+   #~ c1.SaveAs('{OUT}/run{RUN}_{TITLE}_nofit.pdf'.format(OUT=outdir,RUN=run,TITLE=h2d.GetTitle()))
    model.plotOn(frame)
    model.plotOn(frame,RooFit.Components('sig'),RooFit.LineStyle(r.kDashed))
    model.plotOn(frame,RooFit.Components('bkg'),RooFit.LineStyle(r.kDashed))
    frame.Draw()
-   t=add_text(0.6,0.9,0.7,0.9,["Breit-Wigner","Mean = ({0:.3f} #pm {1:.3f}) GeV".format(mean.getValV(),mean.getError()),"Width = ({0:.3f} #pm {1:.3f}) GeV".format(sigma.getValV(),sigma.getError())])
+
+
+   chi2 = RooChi2Var("chi2","chi2",model,dh) ;
+   parameters = model.getParameters(dh);
+   npar = parameters.selectByAttrib("Constant",r.kFALSE).getSize()
+   
+   texts = []
+   if(sigpdf=="bwcrystal"):
+      texts.append(add_text(0.65,0.95,0.7,0.9,["Breit-Wigner","Mean = ({0:.3f} #pm {1:.3f}) GeV".format(mean.getValV(),mean.getError()),"Width = ({0:.3f} #pm {1:.3f}) GeV".format(sigma.getValV(),sigma.getError())]))
+      texts.append(add_text(0.65,0.95,0.4,0.7,["Crystal Ball",
+                                  "Width = ({0:.3f} #pm {1:.3f}) GeV".format(resolution_width.getValV(),resolution_width.getError()),
+                                  "a = {0:.3f} #pm {1:.3f}".format(resolution_a.getValV(),resolution_a.getError()),
+                                  "n = {0:.3f} #pm {1:.3f}".format(resolution_n.getValV(),resolution_n.getError()),
+                                  "",
+                                  "\chi^{{2}}/NDF = {0:.1f} / {1:.0f}".format(chi2.getValV() , npar),
+                                  "N_{{sig}} = {0:.0f}".format(dh.sumEntries() * (1-bkgfrac.getValV()))]))
+                              
+      for t in texts:
+         t.SetFillStyle(1)
+         t.SetFillColor(r.kWhite)
 
 
    c1.SaveAs('{OUT}/run{RUN}_{TITLE}.pdf'.format(OUT=outdir,RUN=run,TITLE=h2d.GetTitle()))
@@ -101,10 +137,11 @@ runs = [ int(x.GetTitle().replace("Run ","")) for x in list(f.Get("DQMData").Get
 base = "DQMData/Run {RUN}/ZCounting/Run summary/Histograms/"
 print runs
 for run in runs:
-   n_fail = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_HLT_fail"),run)
-   n_pass = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_HLT_pass"),run)
+   sigpdf = "bwcrystal"
+   n_fail = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_HLT_fail"),run,sigpdf=sigpdf)
+   n_pass = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_HLT_pass"),run,sigpdf=sigpdf)
    print 'HLT Pass / Fail / Eff: ', n_pass, n_fail, n_pass/(n_pass + n_fail)
 
-   n_fail = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_id_fail"),run)
-   n_pass = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_id_pass"),run)
+   n_fail = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_id_fail"),run,sigpdf=sigpdf)
+   n_pass = do_single_fit(f.Get(base.format(RUN=run)+"h_ee_mass_id_pass"),run,sigpdf=sigpdf)
    print 'ID Pass / Fail / Eff: ', n_pass, n_fail, n_pass/(n_pass + n_fail)
