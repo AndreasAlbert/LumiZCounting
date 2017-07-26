91ce16863c3581eb35fce4678514f021a7bfe774
aalbert <albert@physik.rwth-aachen.de>
2017-07-26 11:45:19 +0200

    run_multiple: Fix subdirectory issue.

--- run_multiple.py
+++ run_multiple.py
@@ -11,12 +11,13 @@ import multiprocessing
 import datetime
 config = os.path.abspath("python/ZCounting_ele_cfg.py")
 tag = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+topdir = os.getcwd()
 
 def run_single(infile):
    ID = "".join(random.sample(string.ascii_uppercase + string.digits, 8))
    print "Running over file {FILE}. ID is {ID}.".format(FILE=infile,ID=ID)
 
-   outdir = "./output/{TAG}/{ID}".format(TAG=tag, ID=ID)
+   outdir = os.path.join(topdir,"output/{TAG}/{ID}".format(TAG=tag, ID=ID))
    if(not os.path.exists(outdir)):
       os.makedirs(outdir)
 
@@ -26,6 +27,7 @@ def run_single(infile):
    with open("log.txt","w") as f:
       p = subprocess.Popen(cmd,stdout=f,stderr=f)
    p.communicate()
+   os.chdir(topdir)
 
 
 def read_files():
@@ -34,7 +36,8 @@ def read_files():
    return lines
 
 def main():
-   files = read_files()[:60]
+
+   files = read_files()
    #~ run_single(files[0])
    print "Running over {NFILE} files. Tag is {TAG}.".format(NFILE=len(files),TAG=tag)
    pool = multiprocessing.Pool(60)
